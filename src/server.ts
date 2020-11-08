23df3c9b28c2d2789d0f5995908a227cea3bfe71
reudismam@gmail.com <reudismam@gmail.com>
2020-11-08 13:00:56 -0300

    parameters.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 
 var server = express();
+server.use(express.json());
 
 server.get("/", (req, res) => {
+    let user = req.body;
+    console.log(user);
     res.json({user: "Reudismam"});
 });
 
